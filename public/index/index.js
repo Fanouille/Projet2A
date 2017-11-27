63e2173354659b0fe2f6e0d40b6c6810bb6c409f
quentinschiavon <quentin.schiavon@depinfonancy.net>
2017-11-27 18:24:43 +0100

    modifications de index pour debut interface

--- public/index/index.js
+++ public/index/index.js
@@ -3,13 +3,27 @@
  */
 
 angular.module('AngularGen')
-    .controller('IndexController', function ($scope){
+    .controller('IndexController', function ($scope,$state){
     	$scope.varGlobal = 0
     	$scope.changeVar = function(id){
     		$scope.varGlobal = id
     	}
+    
+    $scope.openHome = function(){
+  		$state.go('home')
+  	}
 
+  	$scope.openTest = function(){
+  		$state.go('test')
+  	}
 
+  	$scope.recherche=''
+        
+    $scope.$watch('recherche',function(){
+        console.log($scope.recherche)
+    },true)//,true permet de regarder à l'interieur de l'objet
+        //il écoute donc si il y a du changement dans l'objet
+  	
     });
 
 
