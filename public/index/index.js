9a11fba6ce5fe50f971c796af689ee8a168b4403
quentinschiavon <quentin.schiavon@depinfonancy.net>
2018-03-07 18:17:34 +0100

    toolbar retiree

--- public/index/index.js
+++ public/index/index.js
@@ -41,12 +41,6 @@ angular.module('AngularGen')
     }
 
   	$scope.recherche=""
-    /*   
-    $scope.$watch('recherche',function(){
-        console.log($scope.recherche)
-    },true)//,true permet de regarder à l'interieur de l'objet
-        //il écoute donc si il y a du changement dans l'objet
-    */
 
     $scope.objetRecherche=''
     $scope.liste=[]
@@ -64,109 +58,5 @@ angular.module('AngularGen')
         })     
   	}
 
-
-    function PanelGroupsCtrl() {
-      this.settings = {
-        name: 'settings',
-        items: [
-          'Home',
-          'About',
-          'Contact'
-        ]
-      };
-
-      this.menuTemplate = '' +
-          '<div class="menu-panel" md-whiteframe="4">' +
-          '  <div class="menu-content">' +
-          '    <div class="menu-item" ng-repeat="item in ctrl.items">' +
-          '      <button class="md-button">' +
-          '        <span>{{item}}</span>' +
-          '      </button>' +
-          '    </div>' +
-          '    <md-divider></md-divider>' +
-          '    <div class="menu-item">' +
-          '      <button class="md-button" ng-click="ctrl.closeMenu()">' +
-          '        <span>Close Menu</span>' +
-          '      </button>' +
-          '    </div>' +
-          '  </div>' +
-          '</div>';
-
-      $mdPanel.newPanelGroup('toolbar', {
-        maxOpen: 2
-      });
-
-      $mdPanel.newPanelGroup('menus', {
-        maxOpen: 3
-      });
-
-      this.showToolbarMenu = function(event, menu) {
-        var template = this.menuTemplate;
-
-        var position = $mdPanel.newPanelPosition()
-            .relativeTo(event.srcElement)
-            .addPanelPosition(
-              $mdPanel.xPosition.ALIGN_START,
-              $mdPanel.yPosition.BELOW
-            );
-
-        var config = {
-          id: 'toolbar_' + menu.name,
-          attachTo: angular.element(document.body),
-          controller: PanelMenuCtrl,
-          controllerAs: 'ctrl',
-          template: template,
-          position: position,
-          panelClass: 'menu-panel-container',
-          locals: {
-            items: menu.items
-          },
-          openFrom: event,
-          focusOnOpen: false,
-          zIndex: 100,
-          propagateContainerEvents: true,
-          groupName: ['toolbar', 'menus'],
-        };
-
-        $mdPanel.open(config);
-      };
-
-      this.showContentMenu = function(event, menu) {
-        var template = this.menuTemplate;
-
-        var position = $mdPanel.newPanelPosition()
-            .relativeTo(event.srcElement)
-            .addPanelPosition(
-              $mdPanel.xPosition.ALIGN_START,
-              $mdPanel.yPosition.BELOW
-            );
-
-        var config = {
-          id: 'content_' + menu.name,
-          attachTo: angular.element(document.body),
-          controller: PanelMenuCtrl,
-          controllerAs: 'ctrl',
-          template: template,
-          position: position,
-          panelClass: 'menu-panel-container',
-          locals: {
-            items: menu.items
-          },
-          openFrom: event,
-          focusOnOpen: false,
-          zIndex: 100,
-          propagateContainerEvents: true,
-          groupName: 'menus',
-        };
-
-        $mdPanel.open(config);
-      };
-    }
-
-    function PanelMenuCtrl(mdPanelRef) {
-      this.closeMenu = function() {
-        mdPanelRef && mdPanelRef.close();
-      }
-    }
 
 });
