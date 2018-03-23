5ce08488e5591ccd6444c3d222143415ec72c7d2
quentinschiavon <quentin.schiavon@depinfonancy.net>
2018-03-23 15:14:55 +0100

    modification graphiques

--- public/index/index.js
+++ public/index/index.js
@@ -81,4 +81,16 @@ angular.module('AngularGen')
   }
 
   remplirListe();
+
+  //MENU INDEX
+  $scope.topDirections = ['left', 'up'];
+  $scope.bottomDirections = ['down', 'right'];
+
+  $scope.isOpen = false;
+
+  $scope.availableModes = ['md-fling', 'md-scale'];
+  $scope.selectedMode = 'md-fling';
+
+  $scope.availableDirections = ['up', 'down', 'left', 'right'];
+  $scope.selectedDirection = 'down';
 });
